5c3bbec20621b3636556c6794a97483d163cc8c7
didebbo <gianluca.napoletano.93@gmail.com>
2021-08-26 11:56:06 +0200

    .

--- snack-5/js/index.js
+++ snack-5/js/index.js
@@ -1,5 +1,3 @@
-// Esercizio 2
-
 const totaleNumeri = 6;
 const arrayNumeri = [];
 
@@ -24,14 +22,3 @@ buttons[1].onclick = () => {
         }
     }
 }
-
-
-// Esercizio 1
-// Chiedo all'utente totaleNumeri
-// for (let i = 0; i < totaleNumeri; i++) {
-    // let numero;
-    // Chiedo all'utente il medesimo numero fin tanto che sia valido (più o meno).
-    // while (isNaN(numero)) numero = parseInt(prompt("Inserisci numero " + (i + 1)));
-    // Se numero è dispari, lo inserisco nell'array.
-    // if (numero % 2 == 1) arrayNumeri.push(numero);
-// }
